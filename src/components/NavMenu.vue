48d207f48d2956a2a0705969238b8e4779612c13
yccjs <yccjsalvador@gmail.com>
2022-08-01 15:20:15 +0900

    feat: add nav

---
+++ src/components/NavMenu.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="nav">
+    <ButtonComponent text="My Books" />
+    <ButtonComponent text="All Books" />
+  </div>
+</template>
+
+<script>
+import ButtonComponent from "./ButtonComponent";
+export default {
+  name: "NavMenu",
+  components: { ButtonComponent },
+};
+</script>
+
+<style scoped>
+.nav {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding: 20px;
+  width: 100vw;
+}
+</style>
