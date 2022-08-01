06457c88a57d5ec390a69977a765e5695cb2494e
yccjs <yccjsalvador@gmail.com>
2022-08-01 14:49:23 +0900

    feat: add reusable button component

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ButtonComponent",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {},
+  },
+};
+</script>
