381fb501eed93fa53bd45e0f88566249605b1362
yccjs <yccjsalvador@gmail.com>
2022-08-01 14:49:42 +0900

    feat: Add views folder for routing

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <NavMenu />
+  <AllBooks :allBooks="allBooks" />
+</template>
+
+<script>
+import AllBooks from "../components/AllBooks.vue";
+import NavMenu from "../components/NavMenu";
+import axios from "axios";
+
+export default {
+  name: "HomePage",
+  components: { AllBooks, NavMenu },
+
+  data() {
+    return {
+      allBooks: [],
+    };
+  },
+  methods: {
+    async fetchBooks() {
+      let books = await axios.get("http://localhost:9000/books");
+      this.allBooks = books.data;
+    },
+  },
+  created() {
+    this.fetchBooks();
+  },
+};
+</script>
