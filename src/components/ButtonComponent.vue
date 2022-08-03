0300be45d68afdfc926b94dd94796ea7085f7b34
yccjs <yccjsalvador@gmail.com>
2022-08-03 18:47:20 +0900

    chore: edit button styles

--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -10,9 +10,30 @@ export default {
   props: {
     text: String,
     color: String,
+    className: String,
   },
   methods: {
     onClick() {},
   },
 };
 </script>
+
+<style scoped>
+.btn {
+  border: 1px black solid;
+  background: linear-gradient(32.95deg, #786e86 8.75%, #dba6e5 86.64%);
+  padding-top: 8px;
+  padding-bottom: 8px;
+  padding-left: 15px;
+  padding-right: 15px;
+  margin: 4px;
+  border-radius: 4px;
+}
+
+.btn:hover {
+  background: linear-gradient(32.95deg, #e0dde5 8.75%, #a56cb0 86.64%);
+  border: 1px solid purple;
+  cursor: pointer;
+  color: white;
+}
+</style>
